8dd8411232dc106dd09f4cf3fab87e0d63c063b9
MICHAEL KABUTEY <mkabutey@MICHAELs-MacBook-Pro.local>
2019-02-27 13:28:50 +0000

    Models Created

---
+++ app/Http/Controllers/SchoolAccountController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SchoolAccount;
+use Illuminate\Http\Request;
+
+class SchoolAccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SchoolAccount  $schoolAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SchoolAccount $schoolAccount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SchoolAccount  $schoolAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SchoolAccount $schoolAccount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SchoolAccount  $schoolAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SchoolAccount $schoolAccount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SchoolAccount  $schoolAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SchoolAccount $schoolAccount)
+    {
+        //
+    }
+}
